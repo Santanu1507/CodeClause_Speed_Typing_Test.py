30da69cd899f3d370643ddf109dc5ba5e5fd39c3
Santanu Talukdar <77837983+Santanu1507@users.noreply.github.com>
2023-07-07 16:50:05 +0530

    Update Speed_typing_test.py

--- Speed_typing_test.py
+++ Speed_typing_test.py
@@ -1,43 +1,77 @@
+import tkinter as tk
 import time
 
-def typing_test():
-    prompt_text = "The quick brown fox jumps over the lazy dog."
-    print("Type the following text as fast as you can.\n")
-    print(prompt_text)
-    input("Press Enter when you are ready to start...")
+class TypingTest:
+    def __init__(self):
+        self.prompt_text = "The quick brown fox jumps over the lazy dog."
+        self.start_time = 0
+        self.end_time = 0
+        self.elapsed_time = 0
 
-    start_time = time.time()
+        self.root = tk.Tk()
+        self.root.title("Typing Test")
+        self.root.geometry("400x200")
 
-    user_input = input("\nStart typing: ")
+        self.prompt_label = tk.Label(self.root, text="Type the following text as fast as you can:")
+        self.prompt_label.pack()
 
-    end_time = time.time()
+        self.text_label = tk.Label(self.root, text=self.prompt_text, wraplength=380)
+        self.text_label.pack()
 
-    # Calculate the time taken in seconds
-    elapsed_time = end_time - start_time
+        self.start_button = tk.Button(self.root, text="Start", command=self.start_typing_test)
+        self.start_button.pack()
 
-    # Calculate the number of characters typed by the user
-    num_characters_typed = len(user_input)
+        self.stats_label = tk.Label(self.root, text="")
+        self.stats_label.pack()
 
-    # Calculate words per minute (WPM) and accuracy
-    words_per_minute = (num_characters_typed / 5) / (elapsed_time / 60)
-    accuracy = calculate_accuracy(prompt_text, user_input)
+    def start_typing_test(self):
+        self.start_time = time.time()
 
-    print("\nTime taken: {:.2f} seconds".format(elapsed_time))
-    print("Characters typed: {}".format(num_characters_typed))
-    print("Words per minute (WPM): {:.2f}".format(words_per_minute))
-    print("Accuracy: {:.2f}%".format(accuracy))
+        self.prompt_label.destroy()
+        self.start_button.destroy()
 
-def calculate_accuracy(prompt_text, user_input):
-    prompt_words = prompt_text.split()
-    user_words = user_input.split()
+        self.user_input_entry = tk.Entry(self.root)
+        self.user_input_entry.pack()
 
-    correct_words = 0
-    for i in range(min(len(prompt_words), len(user_words))):
-        if prompt_words[i] == user_words[i]:
-            correct_words += 1
+        self.submit_button = tk.Button(self.root, text="Submit", command=self.submit_typing_test)
+        self.submit_button.pack()
 
-    accuracy = (correct_words / len(prompt_words)) * 100
-    return accuracy
+    def submit_typing_test(self):
+        self.end_time = time.time()
+        self.elapsed_time = self.end_time - self.start_time
+
+        user_input = self.user_input_entry.get()
+
+        self.user_input_entry.destroy()
+        self.submit_button.destroy()
+        self.text_label.destroy()
+
+        num_characters_typed = len(user_input)
+        words_per_minute = (num_characters_typed / 5) / (self.elapsed_time / 60)
+        accuracy = self.calculate_accuracy(user_input)
+
+        stats_text = "Time taken: {:.2f} seconds\n".format(self.elapsed_time)
+        stats_text += "Characters typed: {}\n".format(num_characters_typed)
+        stats_text += "Words per minute (WPM): {:.2f}\n".format(words_per_minute)
+        stats_text += "Accuracy: {:.2f}%".format(accuracy)
+
+        self.stats_label.config(text=stats_text)
+
+    def calculate_accuracy(self, user_input):
+        prompt_words = self.prompt_text.split()
+        user_words = user_input.split()
+
+        correct_words = 0
+        for i in range(min(len(prompt_words), len(user_words))):
+            if prompt_words[i] == user_words[i]:
+                correct_words += 1
+
+        accuracy = (correct_words / len(prompt_words)) * 100
+        return accuracy
+
+    def run(self):
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    typing_test()
+    typing_test = TypingTest()
+    typing_test.run()
